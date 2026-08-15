c014275bb4bf61416b9d3821b1a00d73e10e27bc
nelusion <post@maxhoerrmann.de>
2026-08-15 22:48:21 +0200

    Create Bootstrap.java

---
+++ src/main/java/com/nelusion/astraclient/application/Bootstrap.java
@@ -0,0 +1,21 @@
+package com.nelusion.astraclient.application;
+
+import com.nelusion.astraclient.Astra;
+
+public final class Bootstrap {
+
+    private static boolean initialized = false;
+
+    private Bootstrap() {
+    }
+
+    public static void initialize() {
+        if (initialized) {
+            return;
+        }
+
+        initialized = true;
+
+        Astra.services().initialize();
+    }
+}
