cf9767d56239e45f5684217eebde18e10b1f1ef8
nelusion <post@maxhoerrmann.de>
2026-08-15 17:25:01 +0200

    Update AstraClientInitializer.java

--- src/client/java/com/nelusion/astraclient/AstraClientInitializer.java
+++ src/client/java/com/nelusion/astraclient/AstraClientInitializer.java
@@ -1,5 +1,6 @@
 package com.nelusion.astraclient;
 
+import com.nelusion.astraclient.event.ClientEventBridge;
 import com.nelusion.astraclient.core.ClientBootstrap;
 import net.fabricmc.api.ClientModInitializer;
 
@@ -8,5 +9,6 @@ public class AstraClientInitializer implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientBootstrap.initialize();
+        ClientEventBridge.initialize();
     }
 }
