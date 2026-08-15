62d2351fb7c0020afe67ff93d6afdcdc1ac9df31
nelusion <post@maxhoerrmann.de>
2026-08-15 17:16:58 +0200

    Create Event.java

---
+++ src/main/java/com/nelusion/astraclient/event/Event.java
@@ -0,0 +1,14 @@
+package com.nelusion.astraclient.event;
+
+public abstract class Event {
+
+    private boolean cancelled;
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void cancel() {
+        this.cancelled = true;
+    }
+}
