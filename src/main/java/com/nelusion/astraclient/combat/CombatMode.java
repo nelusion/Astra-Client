8bf04b21d0fcfb6e6ebbc1304ff2eae0e78c1964
nelusion <post@maxhoerrmann.de>
2026-08-16 12:09:10 +0200

    Update CombatMode.java

--- src/main/java/com/nelusion/astraclient/combat/CombatMode.java
+++ src/main/java/com/nelusion/astraclient/combat/CombatMode.java
@@ -3,7 +3,7 @@ package com.nelusion.astraclient.combat;
 public enum CombatMode {
 
     ONE_EIGHT("1.8 PvP"),
-    MODERN("1.9+ PvP");
+    ONE_NINE_PLUS("1.9+ PvP");
 
     private final String displayName;
 
@@ -12,6 +12,11 @@ public enum CombatMode {
     }
 
     public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
         return displayName;
     }
 }
