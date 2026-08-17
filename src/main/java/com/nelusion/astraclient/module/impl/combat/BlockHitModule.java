23d710c0db96249fcbfc81403e1f2066484635f0
nelusion <post@maxhoerrmann.de>
2026-08-17 14:26:41 +0200

    Update BlockHitModule.java

--- src/main/java/com/nelusion/astraclient/module/impl/combat/BlockHitModule.java
+++ src/main/java/com/nelusion/astraclient/module/impl/combat/BlockHitModule.java
@@ -1,6 +1,7 @@
 package com.nelusion.astraclient.module.impl.combat;
 
 import com.nelusion.astraclient.combat.CombatMode;
+import com.nelusion.astraclient.event.impl.UpdateEvent;
 import com.nelusion.astraclient.module.Module;
 import com.nelusion.astraclient.module.ModuleCategory;
 
@@ -15,5 +16,14 @@ public final class BlockHitModule extends Module {
     @Override
     public EnumSet<CombatMode> supportedModes() {
         return EnumSet.of(CombatMode.ONE_EIGHT);
+    }
+
+    @Override
+    protected void onEnable() {
+
+        listen(UpdateEvent.class, event -> {
+            System.out.println("[Astra] BlockHit update");
+        });
+
     }
 }
