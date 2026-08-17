c4626284924052cb9aa6cec0abfd749ef9faa3b7
nelusion <post@maxhoerrmann.de>
2026-08-17 14:24:23 +0200

    Update Module.java

--- src/main/java/com/nelusion/astraclient/module/Module.java
+++ src/main/java/com/nelusion/astraclient/module/Module.java
@@ -1,13 +1,21 @@
 package com.nelusion.astraclient.module;
 
 import com.nelusion.astraclient.combat.CombatMode;
+import com.nelusion.astraclient.event.Event;
+import com.nelusion.astraclient.event.EventBus;
+import com.nelusion.astraclient.event.EventListener;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 
 public abstract class Module {
 
     private final String name;
     private final ModuleCategory category;
+
+    private final List<ListenerRegistration<?>> listeners = new ArrayList<>();
+
     private boolean enabled;
 
     protected Module(String name, ModuleCategory category) {
@@ -39,6 +47,7 @@ public abstract class Module {
         if (enabled) {
             onEnable();
         } else {
+            unregisterListeners();
             onDisable();
         }
     }
@@ -47,5 +56,28 @@ public abstract class Module {
     }
 
     protected void onDisable() {
+    }
+
+    protected <T extends Event> void listen(Class<T> eventClass, EventListener<T> listener) {
+        EventBus.register(eventClass, listener);
+        listeners.add(new ListenerRegistration<>(eventClass, listener));
+    }
+
+    private void unregisterListeners() {
+        for (ListenerRegistration<?> registration : listeners) {
+            registration.unregister();
+        }
+
+        listeners.clear();
+    }
+
+    private record ListenerRegistration<T extends Event>(
+            Class<T> eventClass,
+            EventListener<T> listener
+    ) {
+
+        void unregister() {
+            EventBus.unregister(eventClass, listener);
+        }
     }
 }
